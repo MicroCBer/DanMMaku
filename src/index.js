e13ddb67b03a3d521f962c80fab03e00c92bac62
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-07-08 21:21:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 import Settings from './pages/Settings';
 import PluginSettings from './pages/PluginSettings';
 
-const Buffer = require('buffer/').Buffer
+const Buffer = require('buffer').Buffer
 window["Buffer"] = Buffer
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
